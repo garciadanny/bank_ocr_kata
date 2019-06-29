b85d4f7ab02b35ff62943db3c9020ffeb14b1a2e
Danny Garcia <dannygarcia.me@gmail.com>
2019-06-29 16:46:26 -0400

    Add info to script

--- bin/ocr.rb
+++ bin/ocr.rb
@@ -8,5 +8,9 @@ results_path = File.join(
   File.dirname(__FILE__),
   "../results/results_#{Time.now.to_i}"
 )
+
 ocr = OCR.parse_document(document_path, results_path)
+puts "Parsing document located in: #{document_path}..."
+
 ocr.print_results
+puts "Results saved to: #{results_path}."
